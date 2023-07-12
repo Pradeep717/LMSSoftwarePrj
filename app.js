3b179eaa3d5454fbd0d35f282a74025f38ab8839
Pradeep Sandaruwan Udaya Kumara <112861629+Pradeep717@users.noreply.github.com>
2023-07-12 21:54:46 +0530

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const cors = require('cors')
+require("dotenv").config();
 const PORT = process.env.PORT || 5000
 const {MONGO_URI} = require('./keys')
 app.use(express.json())
